83aed9728a7286deb85e458f664ab3ce92f3a753
jlcm <jlcarrion90@gmail.com>
2023-01-12 23:21:17 -0500

    Estructura Pagina 1

--- 02-Componentes/demo/src/app/app.module.ts
+++ 02-Componentes/demo/src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { StudentsComponent } from './components/students/students.component';
+
+import { LoginAdministradorComponent } from './components/login-administrador/login-administrador.component';
+import { LoginEstudianteComponent } from './components/login-estudiante/login-estudiante.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    LoginComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    StudentsComponent,
+
+    LoginAdministradorComponent,
+    LoginEstudianteComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
