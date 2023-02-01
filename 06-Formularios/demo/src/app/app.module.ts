69b17dd35a2581d791b5854d05bd10ece62aef18
jlcm <jlcarrion90@gmail.com>
2023-01-31 20:36:57 -0500

    listo

--- 06-Formularios/demo/src/app/app.module.ts
+++ 06-Formularios/demo/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
---
+++ 07-Angular Material/angularMaterial/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialComponent } from './components/material/material.component';
+import { MaterialModule } from './material.module';
+import { EditarCursoDialogComponent } from './components/editar-curso-dialog/editar-curso-dialog.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MaterialComponent,
+    EditarCursoDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
